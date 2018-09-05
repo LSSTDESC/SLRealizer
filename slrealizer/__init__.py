683c206467fceed1f9033b52927a80cdff121a31
jiwoncpark <jiwon.christine.park@gmail.com>
2018-09-05 15:38:56 -0700

    Fix typo slrealizer --> SLRealizer

--- slrealizer/__init__.py
+++ slrealizer/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 
 __all__ = ['realize_sl', 'realize_sdss', 'realize_om10', 'utils', 'data',]
 # To keep imports like `from slrealizer import OM10Realizer` from breaking
-from .realize_sl import slrealizer
-from .realize_om10 import OM10Realizer
-from .realize_sdss import SDSSRealizer
+from slrealizer.realize_sl import SLRealizer
+from slrealizer.realize_om10 import OM10Realizer
+from slrealizer.realize_sdss import SDSSRealizer
